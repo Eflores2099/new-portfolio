b347359e73190a1d985eca498d3caee73dc1003b
Erik Flores <erik.flores@colorado.edu>
2021-01-25 21:05:22 -0700

    projects and blog

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import image from '../felix-besombes.jpg'
 export default function Home() {
     return (
         <main>
-            <img src={image} alt="blue laptop"className="absolute oject-cover w-full h-full"/>
+            <img src={image} alt="blue laptop"className="absolute object-cover w-full h-full"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8" >
                 <h1 className=" text=6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Bon jour! I'm Erik.</h1>
             </section>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import sanityClient from '../client.js'
 
 
 export default function Projects() {
+    const [projectData, setProjectData] = useState(null);
+
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "project"]{
+            title,
+            date,
+            place,
+            description,
+            project,
+            link,
+            tags
+        }`).then((data) => setProjectData(data))
+        .catch(console.error)
+    }, [])
     return (
-        <div></div>
+        <main className="bg-green-100 min-h-screen p-12">
+            <section className="container mx-auto">
+                <h1 text-5xl flex justify-center>My Projects</h1>
+                <h2></h2>
+                <section>
+                    <article>
+                        <h3></h3>
+                        <div>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <p></p>
+                            <span></span>
+                        </div>
+                    </article>
+                </section>
+            </section>
+        </main>
     )
 }
