5e11e62e1bd05507e7e69b94540bd6a7c42ebced
Erik Flores <erik.flores@colorado.edu>
2021-02-05 19:01:54 -0700

    styling changes to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,11 @@ import image from '../ergita-sela.jpg'
 export default function Home() {
     return (
         <main>
-            <img src={image} alt="Ergita Sela. the white theme" className="absolute object-cover w-full h-full"/>
-            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-8 px-8" >
-                <h1 className=" text=lg text-purple-400 font-medium leading-none lg:leading-snug home-name text-center sm:text-left">Erik Flores. React Developer.</h1>
+            <img src={image} alt="Ergita Sela. the white theme" className="sm:bg-gradient-to-s  md:absolute object-cover w-full h-full "/>
+            {/* <img src={image} alt="Ergita Sela. the white theme" className=" bg-gradient-to-l md:bg-gradient-to-r "/> */}
+            <section className="relative flex-col justify-center min-h-full pt-12 lg:pt-20 px-8" >
+                <h1 className="sm:text-left text-purple-900 font-small leading-none lg:leading-snug home-name text-left">Erik Flores</h1>
+                <h2 className="sm:text-left mt-4 text-purple-900 text-6xl font-small leading-none lg:leading-snug text-left">React Developer</h2>
             </section>
             
         </main>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import {SocialIcon } from 'react-social-icons'
 
 export default function NavBar() {
     return (
-        <header className="bg-green-600">
+        <header className="bg-green-600 bg-opacity-50">
             <div className="container mx-auto flex justify-between">
                 <nav className='flex'>
                     <NavLink 
@@ -17,26 +17,26 @@ export default function NavBar() {
                     <NavLink 
                         to= '/projects'
                         activeClassName='text-green-300 bg-green-100'
-                        className='inline-flex items-center py-3 px-3 my-6 rounded text-purple-500 hover:text-red-100'>
+                        className='inline-flex items-center py-3 px-3 my-6 rounded text-purple-900 hover:text-red-100'>
                         Projects
                     </NavLink>
                     <NavLink 
                         to= '/posts'
                         activeClassName='text-green-300 bg-green-100'
-                        className='inline-flex items-center py-3 px-3 my-6 rounded text-purple-500 hover:text-red-100'>
+                        className='inline-flex items-center py-3 px-3 my-6 rounded text-purple-900 hover:text-red-100'>
                         Blog Posts
                     </NavLink>
                     <NavLink    
                         to ='/about'
                         activeClassName='text-green-300 bg-green-100'
-                        className='inline-flex items-center py-3 px-3 my-6 rounded text-purple-500 hover:text-red-100'>
+                        className='inline-flex items-center py-3 px-3 my-6 rounded text-purple-900 hover:text-red-100'>
                         About Me
                     </NavLink>
                 </nav>
                 <div className="inline-flex py-3 px-3 my-6">
                     <SocialIcon url="https://www.linkedin.com/in/eflores2099/" className="mr-4" target="_blank" fgColor='#fff' style={{height:35, width: 35}}/>
                     <SocialIcon url="https://github.com/Eflores2099" className="mr-4" target="_blank" fgColor='#fff' style={{height:35, width: 35}}/>
-                    <SocialIcon url="https://twitter.com/Erik96636380" className="mr-4" target="_blank" fgColor='#fff' style={{height:35, width: 35}}/>
+                    {/* <SocialIcon url="https://twitter.com/Erik96636380" className="mr-4" target="_blank" fgColor='#fff' style={{height:35, width: 35}}/> */}
                 </div>
             </div>
         </header>
